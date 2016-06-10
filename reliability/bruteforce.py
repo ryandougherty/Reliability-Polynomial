eb7bc25afb7002a47ccebb97e32f10fc6a0bff2b
Ryan Dougherty <ryan.dougherty@asu.edu>
2016-06-10 16:51:28 -0700

    Added start of brute force functions

---
+++ reliability/bruteforce.py
@@ -0,0 +1,27 @@
+from sympy.parsing.sympy_parser import parse_expr
+
+def relpoly(G):
+	rel = _recursive(G)
+	return parse_expr(rel)
+
+def _recursive(G):
+	if len(G.nodes()) <= 1:
+		return '(1)'
+	elif nx.number_connected_components(G) > 1:
+		return None
+	else:
+		e = random.choice(G.edges())
+		H = G.copy()
+		H.remove_edge(*e)
+		H1 = nx.contracted_edge(G,e)
+		print(len(G.nodes()), len(H1.nodes()))
+		first = _recursive(H1)
+		second = _recursive(H)
+		if first and second:
+			return '(p*'+first+')+(1-p)*('+second+')'
+		elif first:
+			return '(p*'+first+')'
+		elif second:
+			return '(1-p)*('+second+')'
+		else:
+			return ''
