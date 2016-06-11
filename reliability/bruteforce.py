41f344b78a5f0462518567f8af96f25b0e8b1255
Ryan Dougherty <ryan.dougherty@asu.edu>
2016-06-10 22:44:42 -0700

    Switched to Sympy, fixed bugs with parallel edges being deleted, and cleaned up the code.

--- reliability/bruteforce.py
+++ reliability/bruteforce.py
@@ -1,29 +1,33 @@
-from sympy.parsing.sympy_parser import parse_expr
+import networkx as nx
+import random
+import sympy
 
 p = sympy.symbols('p')
 
 def relpoly(G):
-	rel = _recursive(G)
-	return parse_expr(rel)
+	H = nx.MultiGraph(G)
+	rel = _recursive(H)
+	return sympy.simplify(rel)
 
 def _recursive(G):
-	if len(G.nodes()) <= 1:
-		return '(1)'
-	elif nx.number_connected_components(G) > 1:
-		return None
-	else:
+	# If the graph is not connected, then it has a rel poly of 0
+	if not nx.is_connected(G):
+		return sympy.Poly(0, p)
+
+	# if # edges > 0, then we perform the two subcases of the 
+	# 	Factoring theorem.
+	if len(G.edges()) > 0:
 		e = random.choice(G.edges())
-		H = G.copy()
-		H.remove_edge(*e)
-		H1 = nx.contracted_edge(G,e)
-		print(len(G.nodes()), len(H1.nodes()))
-		first = _recursive(H1)
-		second = _recursive(H)
-		if first and second:
-			return '(p*'+first+')+(1-p)*('+second+')'
-		elif first:
-			return '(p*'+first+')'
-		elif second:
-			return '(1-p)*('+second+')'
-		else:
-			return ''
+		contracted = nx.contracted_edge(G, e, self_loops=False)
+		G.remove_edge(*e)
+		rec_deleted = _recursive(G)
+		rec_contracted = _recursive(contracted)
+		s = sympy.Poly(p)*(rec_contracted) + sympy.Poly(1-p)*(rec_deleted)
+		return s
+
+	# Otherwise, we only have 0 edges and 1 vertex, which is connected,
+	# 	so we return 1.
+	return sympy.Poly(1, p)
+
+
+print(relpoly(nx.petersen_graph()))
